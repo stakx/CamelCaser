e33646ae44ff93e67d5b2a0a02e70f03ff0b1653
stakx <stakx@eml.cc>
2018-09-09 12:15:48 +0200

    Empty string

--- CamelCaser.Tests/NameTests.cs
+++ CamelCaser.Tests/NameTests.cs
@@ -4,5 +4,10 @@ namespace CamelCaser
 {
     public class NameTests
     {
+        [Fact]
+        public void Empty_string_is_returned_unchanged()
+        {
+            Assert.Equal("", "".ToLowerCamelCase());
+        }
     }
 }
--- CamelCaser/Name.cs
+++ CamelCaser/Name.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Diagnostics;
+
 namespace CamelCaser
 {
     public static class Name
     {
         public static string ToLowerCamelCase(this string name)
         {
+            Debug.Assert(name != null);
+
+            if (name.Length == 0)
+            {
+                return name;
+            }
+
             throw new NotImplementedException();
         }
     }
